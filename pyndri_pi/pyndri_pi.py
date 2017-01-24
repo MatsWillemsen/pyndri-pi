4136af77383a7c88f2417cdf4c7f9428050c091f
Mats Willemsen <mats.willemsen@gmail.com>
2017-01-24 14:18:57 +0100

    term_count() uses term token, not id

--- pyndri_pi/pyndri_pi.py
+++ pyndri_pi/pyndri_pi.py
@@ -10,6 +10,9 @@ class Index:
         self.term_index = open(os.path.join(location, 'term_index'),'rb')
         self.dictionary = open(os.path.join(location,'dictionary'), 'rb')
         self._fill_dictionary()
+        self.cache = {
+            'term_collection' : {}
+        }
     
     def load_index(self, location):
         manifest = namedtuple('manifest', 'document_count unique_terms total_terms')
@@ -53,7 +56,14 @@ class Index:
     
     def term_count(self, term):
         return self.term_frequencies[term]
-
+"""
+    def term_count_collection(self, term):
+        if term in self.cache['term_collection']:
+            return self.cache['term_collection'][term]
+        else:
+            self.cache['term_collection'][term] = sum(document.count(term) for document in self.documents())
+            return self.cache['term_collection'][term]
+"""
     def _fill_dictionary(self):
         self.token2id = {}
         self.id2token = {}
@@ -66,10 +76,11 @@ class Index:
             self.token2id[token] = x
             self.id2token[x] = token
             self.id2df[x] = id_df
-            self.term_frequencies[x] = termcount
+            self.term_frequencies[token] = termcount
 
     def get_dictionary(self):
         return self.token2id, self.id2token, self.id2df
-    
+    def get_term_frequency(self, term):
+        return self.term_frequencies[term]
     def get_term_frequencies(self):
         return self.term_frequencies
